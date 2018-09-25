67bde23aaf5055fe4db5ee787350779c06de5d7f
DESKTOP-T5GT484\user <lee.mingliang@outlook.com>
2018-09-25 22:55:24 +0800

    updates

--- Traffic.m
+++ Traffic.m
@@ -1,6 +1,6 @@
 function  Traffic
 num_particles= 10;
-initial = zeros(1,num_particles*4)
+initial = zeros(1,num_particles*4);
 for i = 1:num_particles
     initial((i-1)*4+1) = normrnd(100,50);
     initial((i-1)*4+3) = normrnd(100,50);
@@ -11,18 +11,17 @@ len = length(y);
 figure(1)
 for i = 1:len
     hold off
-    plot(y(i,1),y(i,3),'o')
-    hold on
-    plot(y(i,5),y(i,7),'o')
-    hold off
-    axis([0 1e4 0 1e4])
+    for j = 1:num_particles
+        plot(y(i,(j-1)*4+1),y(i,(j-1)*4+3),'o')
+        hold on
+    end
+    axis([0 1e3 0 1e3])
     drawnow
 end
-
 end
 
 function ret = movement(t,z)
-num_particles = length(z)/4
+num_particles = length(z)/4;
 ret=zeros(1,length(z));
 for i = 1:num_particles
     ret(1+(i-1)*4) = z(2+(i-1)*4);
