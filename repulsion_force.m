2756cfe6a456fb0077d75e5aad4e655389fd9eca
DESKTOP-T5GT484\user <lee.mingliang@outlook.com>
2018-09-26 19:08:00 +0800

    Update on repulsion force

--- repulsion_force.m
+++ repulsion_force.m
@@ -6,5 +6,11 @@ function ret = repulsion_force(positions,i)
     for i = 1:length(relative_positions)
         relative_positions(i,:) = relative_positions(i,:) - particle;
     end
+    dist = sqrt(relative_positions(:,1).^2 + relative_positions(:,2).^2);
+    angle = atan2(relative_positions(:,2),relative_positions(:,1));
+    force = -10./(dist.^2+0.01);
+    xforce = cos(angle).*force;
+    yforce = sin(angle).*force;
     
+    ret = [sum(xforce) sum(yforce)];
 end
